5c718c0b7aaece5504e933e51fa21145b6f92e67
Sagar Virani <sagarvirani@gmail.com>
2024-05-22 10:50:08 +0530

    wip - adding images to the product grid.

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
@@ -16,8 +16,12 @@ import { authenticate } from "../shopify.server";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
 
+const encodeGlobalId = (type: string, id: any) => {
+  return String(`gid://shopify/${type}/${id}`);
+};
+
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
+   const { admin } = await authenticate.admin(request);
 
   const response = await fetch(
     `https://elysium-homedecor.myshopify.com/admin/api/2024-04/draft_orders.json`,
@@ -33,7 +37,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const draftOrders = await response.json();
 
   const groupedCustomerDetails: { [key: string]: any } = {};
-
+  
   draftOrders.draft_orders.forEach((order: any) => {
     const customer = order.customer;
     if (customer) {
@@ -48,17 +52,73 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
           orders: [],
         };
       }
+      const variantIds: string[] = [];
       groupedCustomerDetails[customerId].orders.push({
         orderId: order.id,
+        orderName: order.name,
         createdAt: order.created_at,
-        lineItems: order.line_items.map((item: any) => ({
-          title: item.title,
-          quantity: item.quantity,
-          price: item.price,
-        })),
+        lineItems: order.line_items.map((item: any) => {
+          const variantId = item.variant_id.toString();
+          variantIds.push(encodeGlobalId("ProductVariant", variantId));
+          const discountValueType = item.applied_discount?.value_type;
+          const formattedDiscountValueType =
+            discountValueType === "percentage" ? "%" : discountValueType;
+          const discountAmount = item.applied_discount?.amount || 0;
+          const finalPrice =
+            item.quantity * item.price -
+            (discountValueType === "%"
+              ? (item.price * item.quantity * item.applied_discount.value) / 100
+              : discountAmount);
+
+          return {
+            variantId: variantId,
+            variantIds: variantIds,
+            title: item.title,
+            unitPrice: item.price,
+            quantity: item.quantity,
+            discount: item.applied_discount
+              ? `${item.applied_discount.value} ${formattedDiscountValueType}`
+              : "-",
+            finalPrice: finalPrice.toFixed(2),
+            imageUrl: null,
+          };
+        }),
       });
     }
   });
+
+  // const variantResponse = await admin.graphql(
+  //   `#graphql query ($variantIds: [ID!]!) {
+  //     nodes(ids: $variantIds) {
+  //       ... on ProductVariant {
+  //         id
+  //         image {
+  //           url
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+
+  // console.log("variant data=", variantResponse);
+
+  // const variantImages: { [key: string]: string } = {};
+  // variantData.nodes.forEach((variant: any) => {
+  //   if (variant && variant.image) {
+  //     variantImages[variant.id] = variant.image.url;
+  //   }
+  // });
+
+  // // Assign images to line items
+  // Object.values(groupedCustomerDetails).forEach((customer: any) => {
+  //   customer.orders.forEach((order: any) => {
+  //     order.lineItems.forEach((item: any) => {
+  //       item.imageUrl =
+  //         variantImages[encodeGlobalId("ProductVariant", item.variantId)] ||
+  //         null;
+  //     });
+  //   });
+  // });
 
   const customerDetails = Object.values(groupedCustomerDetails);
 
@@ -90,6 +150,7 @@ export default function Index() {
   const [customerDetails, setCustomerDetails] = useState(
     initialCustomerDetails,
   );
+  console.log('details=', customerDetails);
 
   const options = customerDetails.map((customer: any) => ({
     label: `${customer.firstName} ${customer.lastName}`,
@@ -138,31 +199,65 @@ export default function Index() {
 
     // Add customer details
     if (selectedCustomerDetails) {
+      doc.setFontSize(12);
       doc.text(
         `Customer: ${selectedCustomerDetails.firstName} ${selectedCustomerDetails.lastName}`,
-        10,
-        10,
+        15,
+        20,
       );
-      doc.text(`Email: ${selectedCustomerDetails.email}`, 10, 20);
-      doc.text(`Phone: ${selectedCustomerDetails.phone}`, 10, 30);
+      doc.text(`Email: ${selectedCustomerDetails.email}`, 15, 30);
+      doc.text(`Phone: ${selectedCustomerDetails.phone}`, 15, 40);
     }
 
     // Add order details
     if (selectedOrder) {
-      doc.text(`Order ID: ${selectedOrder.orderId}`, 10, 40);
-      doc.text(`Date: ${formatDate(selectedOrder.createdAt)}`, 10, 50);
+      doc.setFontSize(14);
+      doc.text(`Order ID: ${selectedOrder.orderId}`, 15, 60);
+      doc.text(`Order Name: ${selectedOrder.orderName}`, 15, 70);
+      doc.text(`Date: ${formatDate(selectedOrder.createdAt)}`, 15, 80);
 
       // Add line items
-      const lineItems = selectedOrder.lineItems.map((item: any) => [
-        item.title,
-        item.quantity,
-        item.price,
-      ]);
+      const lineItems = selectedOrder.lineItems.map(
+        (item: any, index: number) => [
+          item.title,
+          item.unitPrice,
+          item.quantity,
+          item.discount,
+          item.discount == "-"
+            ? `${(item.unitPrice * item.quantity).toFixed(2)}`
+            : item.finalPrice,
+        ],
+      );
+
       autoTable(doc, {
-        head: [["Title", "Quantity", "Price"]],
+        head: [["Title", "Price/Unit", "Quantity", "Discount", "Final Price"]],
         body: lineItems,
-        startY: 60,
+        startY: 90,
+        margin: { top: 85 },
       });
+      // Get the finalY value after the autoTable is rendered
+      let finalY = (doc as any).lastAutoTable.finalY || 0;
+
+      // Calculate total quantity
+      const totalQuantity = selectedOrder.lineItems.reduce(
+        (total: number, item: any) => total + item.quantity,
+        0,
+      );
+
+      // Calculate total final price
+      const totalFinalPrice = selectedOrder.lineItems
+        .reduce(
+          (total: number, item: any) => total + parseFloat(item.finalPrice),
+          0,
+        )
+        .toFixed(2);
+
+      doc.text("Total", 15, finalY + 10);
+      // Add total quantity at the end of the Quantity column
+      doc.text(`${totalQuantity}`, 99, finalY + 10);
+
+      // Add total final price at the end of the Final Price column
+      doc.text(`${totalFinalPrice}`, 154, finalY + 10);
     }
 
     // Format the createdAt date for the filename
@@ -172,10 +267,11 @@ export default function Index() {
     );
 
     doc.save(
-      `${selectedCustomerDetails.firstName} ${selectedCustomerDetails.lastName} ${formattedDate}.pdf`,
+      `${selectedOrder.orderName} - ${selectedCustomerDetails.firstName} ${selectedCustomerDetails.lastName} ${formattedDate}.pdf`,
     );
   };
 
+  const dataTableRef = useRef(null);
   return (
     <Page>
       <TitleBar title="Wishlist App" />
@@ -211,6 +307,9 @@ export default function Index() {
                       <strong>Order Id:</strong> {selectedOrder.orderId}
                     </p>
                     <p>
+                      <strong>Order Name:</strong> {selectedOrder.orderName}
+                    </p>
+                    <p>
                       <strong>Email:</strong> {selectedCustomerDetails.email}
                     </p>
                     <p>
@@ -225,13 +324,62 @@ export default function Index() {
         {selectedOrder && (
           <>
             <Card>
-              <DataTable
-                columnContentTypes={["text", "numeric", "numeric"]}
-                headings={["Title", "Quantity", "Price"]}
-                rows={selectedOrder.lineItems.map((item: any) =>
-                  Object.values(item),
-                )}
-              />
+              <div ref={dataTableRef}>
+                <DataTable
+                  columnContentTypes={[
+                    "text",
+                    "numeric",
+                    "numeric",
+                    "numeric",
+                    "numeric",
+                  ]}
+                  headings={[
+                    "Title",
+                    "Price/Unit",
+                    "Quantity",
+                    "Discount",
+                    "Final Price",
+                  ]}
+                  rows={selectedOrder.lineItems.map(
+                    (item: any, index: number) => [
+                      item.title,
+                      item.unitPrice,
+                      item.quantity,
+                      <div key={index}>{item.discount}</div>,
+                      <div key={index}>
+                        {item.discount !== "-" && (
+                          <span style={{ textDecoration: "line-through" }}>
+                            {(item.unitPrice * item.quantity).toFixed(2)}
+                          </span>
+                        )}
+                        <div>
+                          {item.discount !== "-"
+                            ? `${item.finalPrice}`
+                            : item.finalPrice}
+                        </div>
+                      </div>,
+                    ],
+                  )}
+                  totals={[
+                    "", // Title column doesn't have a total
+                    "", // Price/Unit column doesn't have a total
+                    selectedOrder.lineItems.reduce(
+                      (totalQuantity: number, item: any) =>
+                        totalQuantity + item.quantity,
+                      0,
+                    ), // Total quantity
+                    "", // Discount column doesn't have a total
+                    selectedOrder.lineItems
+                      .reduce(
+                        (totalPrice: number, item: any) =>
+                          totalPrice + parseFloat(item.finalPrice),
+                        0,
+                      )
+                      .toFixed(2), // Total final price
+                  ]}
+                  showTotalsInFooter
+                />
+              </div>
             </Card>
           </>
         )}
